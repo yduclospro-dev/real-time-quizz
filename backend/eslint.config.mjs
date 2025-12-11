406ee30ace71ba4680cd2c7e36669537ab7fe222
yduclos <yduclos@sqli.com>
2025-12-11 18:05:11 +0100

    chore: update configuration files

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -28,8 +28,12 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'off',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off',
     },
   },
 );
